0492cce960120ae85a522dccb8a56af359a69694
calidion <calidion@gmail.com>
2019-12-18 01:01:50 +0800

    updated

--- __tests__/hash-spec.ts
+++
@@ -1,8 +0,0 @@
-import { Hash } from "../src/hash";
-import { HASH_LENGTH } from "../src/types";
-
-test("Should have Greeter available", () => {
-  expect(
-    Hash.from(Buffer.from("Hello world!")).length === HASH_LENGTH
-  ).toBeTruthy();
-});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export * from "./types";
-export * from "./hash";
 export * from "@vigcoin/neon";
